5c288194c204d21a40f6e1f2ba799ef8a678bd00
Tom Manner <tsmanner@us.ibm.com>
2022-08-03 18:03:11 -0400

    Started messing around with notcurses and a TUI

--- build.zig
+++ build.zig
@@ -33,6 +33,59 @@ fn addPackageTests(
     test_step.dependOn(&test_cmd.step);
 }
 
+fn notcurses(
+    builder: *Builder,
+    target: std.zig.CrossTarget,
+    mode: std.builtin.Mode,
+) !void {
+    std.ChildProcess.init()
+
+    const lib = builder.addStaticLibrary("notcurses", null);
+    lib.setTarget(target);
+    lib.setBuildMode(mode);
+    lib.addCSourceFiles(
+        &.{
+            "../notcurses/src/lib/automaton.c",
+            "../notcurses/src/lib/banner.c",
+            "../notcurses/src/lib/blit.c",
+            "../notcurses/src/lib/debug.c",
+            "../notcurses/src/lib/direct.c",
+            "../notcurses/src/lib/fade.c",
+            "../notcurses/src/lib/fd.c",
+            "../notcurses/src/lib/fill.c",
+            "../notcurses/src/lib/gpm.c",
+            "../notcurses/src/lib/in.c",
+            "../notcurses/src/lib/kitty.c",
+            "../notcurses/src/lib/layout.c",
+            "../notcurses/src/lib/linux.c",
+            "../notcurses/src/lib/menu.c",
+            "../notcurses/src/lib/metric.c",
+            "../notcurses/src/lib/mice.c",
+            "../notcurses/src/lib/notcurses.c",
+            "../notcurses/src/lib/plot.c",
+            "../notcurses/src/lib/progbar.c",
+            "../notcurses/src/lib/reader.c",
+            "../notcurses/src/lib/reel.c",
+            "../notcurses/src/lib/render.c",
+            "../notcurses/src/lib/selector.c",
+            "../notcurses/src/lib/sixel.c",
+            "../notcurses/src/lib/sprite.c",
+            "../notcurses/src/lib/stats.c",
+            "../notcurses/src/lib/tabbed.c",
+            "../notcurses/src/lib/termdesc.c",
+            "../notcurses/src/lib/tree.c",
+            "../notcurses/src/lib/unixsig.c",
+            "../notcurses/src/lib/util.c",
+            "../notcurses/src/lib/visual.c",
+            "../notcurses/src/lib/windows.c",
+        },
+        &.{},
+    );
+    lib.addIncludeDir("../notcurses/src/lib");
+    lib.linkLibC();
+    lib.install();
+}
+
 pub fn build(builder: *Builder) !void {
     // Standard target options allows the person running `zig build` to choose
     // what target to build for. Here we do not override the defaults, which
@@ -44,13 +97,17 @@ pub fn build(builder: *Builder) !void {
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
     const mode = builder.standardReleaseOptions();
 
+    try notcurses(builder, target, mode);
+
     const util = Pkg{ .name = "util", .path = .{ .path = "src/util/package.zig" } };
     const web = Pkg{ .name = "web", .path = .{ .path = "src/web/package.zig" }, .dependencies = &[_]Pkg{util} };
+    const tui = Pkg{ .name = "tui", .path = .{ .path = "src/tui/package.zig" }, .dependencies = &[_]Pkg{util} };
     const dnd = Pkg{ .name = "dnd", .path = .{ .path = "src/dnd/package.zig" }, .dependencies = &[_]Pkg{ util, web } };
 
     const packages = [_]Pkg{
         util,
         web,
+        tui,
         dnd,
     };
 
